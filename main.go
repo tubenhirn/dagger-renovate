831faeb850866666c374b6c58015394749261a78
Johannes Stang <tubenhirn@gmail.com>
2023-02-27 20:17:39 +0100

    fix: fix semver version string

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ type image struct {
 var renovateImage = image{
 	Name: "renovate/renovate",
 	//# renovate: datasource=docker depName=renovate/renovate versioning=docker
-	Version: "34.153",
+	Version: "34.153.0",
 }
 
 func renovate(ctx context.Context, platform string) error {
